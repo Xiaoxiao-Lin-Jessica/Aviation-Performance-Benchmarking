049b580343289ae7f8aff26983cabcfa896e7809
Yang Zou <yzou9073@uni.sydney.edu.au>
2023-09-14 23:12:41 +1000

    Revert "Revert "Login jianan lu""

---
+++ src/main/java/org/capstone/service/UserService.java
@@ -0,0 +1,31 @@
+package org.capstone.service;
+import org.capstone.LoginCallback;
+import org.capstone.repository.UserDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Jianan Lu
+ * @version 2.0
+ */
+@Service
+public class UserService implements UserServiceInterface{
+    @Autowired
+    private UserDAO userDao;
+    @Override
+    public boolean login(String email, String password) {
+        boolean[] ifSuccess = new boolean[0];
+        userDao.login(email, password, new LoginCallback() {
+            @Override
+            public void onLoginResult(boolean success) {
+                if (success) {
+                    ifSuccess[0] = true;
+                }
+                else{
+                    ifSuccess[0] = false;
+                }
+            }
+        });
+        return ifSuccess[0];
+    }
+}
