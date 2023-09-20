03bbf48a238146e86c3c0cf8f68e623e93408b2d
Xiaoxiao Lin <xlin9399@uni.sydney.edu.au>
2023-09-20 18:37:36 +1000

    Update user login page + package.json
    User login page:
    Front-end code done, local test all good.
    also install react-router-dom in package.json.
    Also change SignInSide.jsx to UserLogin.jsx for better understanding.

--- frontend/capstone-project/src/App.js
+++ frontend/capstone-project/src/App.js
@@ -1,32 +1,17 @@
 import react from "react";
-import SignInSide from "./components/SignInSide";
+import UserLogin from "./components/UserLogin";
 import Dashboard from "./components/DashBoard";
-import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-    return (
-        /*<div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>*/
-        <div>
-            <h1>Capstone Project</h1>
-            <Dashboard />
-        </div>
-    );
+  return (
+    <Router>
+      <Routes>
+          <Route path="/login" element={<UserLogin />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
