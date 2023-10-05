47f8f97a08fd218ddd9648a2d80da1f0e79ce695
Xiaoxiao Lin <xlin9399@uni.sydney.edu.au>
2023-10-06 03:07:36 +1100

    Update power bi work css, as it wont be clash with navbar
    the power bi work wont be block the navbar

--- frontend/capstone-project/src/components/ForRoute/RouteOverview.jsx
+++ frontend/capstone-project/src/components/ForRoute/RouteOverview.jsx
@@ -46,14 +46,14 @@ function RouteOverview() {
     
         
     return (
+        <div>
+        <Navbar/>
         <PowerBIEmbed
             embedConfig={{
                 type: "report", // Supported types: report, dashboard, tile, visual, qna, paginated report and create
-                id: "xxx", //new power bi item = change here
-                embedUrl:
-                    "xxx", //new power bi item = change here
-                accessToken:
-                    "xxx", //new power bi item = change here
+                id: "6b16fb01-7dab-4978-9312-9ba5eaa7d28c", //new power bi item = change here
+                embedUrl: "https://app.powerbi.com/reportEmbed?reportId=6b16fb01-7dab-4978-9312-9ba5eaa7d28c&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly9XQUJJLVNPVVRILUVBU1QtQVNJQS1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldCIsImVtYmVkRmVhdHVyZXMiOnsidXNhZ2VNZXRyaWNzVk5leHQiOnRydWV9fQ%3d%3d", //new power bi item = change here
+                accessToken: "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyIsImtpZCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyJ9.eyJhdWQiOiJodHRwczovL2FuYWx5c2lzLndpbmRvd3MubmV0L3Bvd2VyYmkvYXBpIiwiaXNzIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQvN2JlOTNiYTctNDQ4Mi00OWQwLWE1MTItN2M2ODE4MDk2ZTMzLyIsImlhdCI6MTY5NjUxOTk2MSwibmJmIjoxNjk2NTE5OTYxLCJleHAiOjE2OTY1MjQzMDUsImFjY3QiOjAsImFjciI6IjEiLCJhaW8iOiJBVFFBeS84VUFBQUFEUTJBMDE3bGplb1RYTzJRdDRVVGpRVHpORHhUY2F3aUI2KzdzMTB4Z0lVeHJLcHIzTkRyREVXMXNXTDFlbVV5IiwiYW1yIjpbInB3ZCIsInJzYSJdLCJhcHBpZCI6Ijg3MWMwMTBmLTVlNjEtNGZiMS04M2FjLTk4NjEwYTdlOTExMCIsImFwcGlkYWNyIjoiMCIsImRldmljZWlkIjoiNmE4YWU3OTAtYWRlYy00YjViLWI4ODctY2NlN2NhNDUxYTU2IiwiZmFtaWx5X25hbWUiOiJMaW4iLCJnaXZlbl9uYW1lIjoiWGlhb3hpYW8iLCJpcGFkZHIiOiIyNDAzOjU4MDc6NzJjNTowOmZjODQ6NTM0YTpkZWQ5OjdiYTYiLCJuYW1lIjoiWGlhb3hpYW8gTGluIiwib2lkIjoiOTYzZjYxYTAtMWZlNS00ZWRlLWJiMGQtOTRmNDkxNjhhZmQxIiwicHVpZCI6IjEwMDMyMDAyMzU3RDFCNzUiLCJyaCI6IjAuQVFvQXB6dnBlNEpFMEVtbEVueG9HQWx1TXdrQUFBQUFBQUFBd0FBQUFBQUFBQUFLQUJvLiIsInNjcCI6InVzZXJfaW1wZXJzb25hdGlvbiIsInNpZ25pbl9zdGF0ZSI6WyJrbXNpIl0sInN1YiI6IloySjRGeGgtc3J6ejVwbGFZVWRHV2VaV3NISlFPeGU1WEtjWGk4TWtkNnciLCJ0aWQiOiI3YmU5M2JhNy00NDgyLTQ5ZDAtYTUxMi03YzY4MTgwOTZlMzMiLCJ1bmlxdWVfbmFtZSI6InhsaW45Mzk5QHVuaS5zeWRuZXkuZWR1LmF1IiwidXBuIjoieGxpbjkzOTlAdW5pLnN5ZG5leS5lZHUuYXUiLCJ1dGkiOiJjZGE2X2lwaURFU2ZDWXcxS05GckFBIiwidmVyIjoiMS4wIiwid2lkcyI6WyJiNzlmYmY0ZC0zZWY5LTQ2ODktODE0My03NmIxOTRlODU1MDkiXX0.mmso2UgTzT8N4NiUdXgWA7MkyrPLfhjgC5WSwH89BUybI-qgHf7SkRFBoUCPE6WsLti-831dT1YYAqwBoOhxyg3U6WH-8b73aiOP63RHgWPo6d9cVq5TU_mVAOdUhOG_a6ipHg0E93dUeDcQfkkdFxq5wuR7WjY66Db2yqODWJVVSgTgaArdgRw1ZSRx2JshKXOufDr6yzlA4LrpQkyqqq3h1NR8g97PoJUIhEagwhhtjH_jDuVoqLLMHhojd8Agg0fHrN-iqLFzhFhNaNoGIe2GxObdpA5Lud4R9UuiMytdsIYaw3jV-YRCHxjwPB1ABSOewUxNmIRahWX9xIqUrQ", //new power bi item = change here
                 tokenType: models.TokenType.Aad, // Use models.TokenType.Aad for SaaS embed
                 settings: {
                     panes: {
@@ -94,7 +94,10 @@ function RouteOverview() {
                 window.report = embeddedReport;
             }}
         />
+        </div>
+
     );
+}
 }
 
 export default RouteOverview;
